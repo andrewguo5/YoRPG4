3ee441b5c572965cb0f4dbfad77a5c313e1c7f84
andrewguo5 <andrew.guo5@gmail.com>
2013-11-17 22:04:58 -0500

    Update Mage.java

--- Mage.java
+++ Mage.java
@@ -14,7 +14,15 @@ public class Mage extends Character {
     _name = newName;
   }
   
-  /*
-  methods here
-  */
+  //The mage casts a magical barrier that has a 66% chance to nullify all damage, however, the mage halves their offensive power
+  public void specialize() {
+    if (Math.random() < 0.66) 
+      _def *= 1000;
+    _atk = 0.4
+    }
+  
+  public void normalize() {
+      _def = 12;
+      _atk = 0.8;
+  }
 }
