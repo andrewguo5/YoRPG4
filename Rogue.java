ad21059ff891d16659b239fa263cf490816a8124
andrewguo5 <andrew.guo5@gmail.com>
2013-11-17 21:40:52 -0500

    Update Rogue.java

---
+++ Rogue.java
@@ -0,0 +1,20 @@
+public class Rogue extends Character {
+  
+  private String _name;
+  
+  public Rogue () {
+      _hp = 90;
+      _str = 30;
+      _def = 14;
+      _atk = 0.35;
+  }
+  
+  public Archer (String newName) {
+    this();
+    _name = newName;
+  }
+  
+  /*
+  methods here
+  */
+}
